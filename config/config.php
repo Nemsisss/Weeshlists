404a853617058d830280a56f95141967e7fc4223
nemsissshah <nemsissshahbazian@gmail.com>
2022-12-19 00:31:40 -0800

    updated

--- config/config.php
+++ config/config.php
@@ -1,9 +1,14 @@
 <?php
 
-define('DB_HOST', '303.itpwebdev.com');
-define('DB_USER', 'nemsisss_db_user');
-define('DB_PASS', 'uscitp2022');
-define('DB_NAME', 'nemsisss_wishlists_db');
+// define('DB_HOST', '303.itpwebdev.com');
+// define('DB_USER', 'nemsisss_db_user');
+// define('DB_PASS', 'uscitp2022');
+// define('DB_NAME', 'nemsisss_wishlists_db');
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+define('DB_HOST', $cleardb_url['host']);
+define('DB_USER', $cleardb_url['user']);
+define('DB_PASS', $cleardb_url['pass']);
+define('DB_NAME', substr($cleardb_url['path'],1));
 session_start();
 
 ?>
