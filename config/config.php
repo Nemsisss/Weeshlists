3bc067615e887ec117411c31a67943ff686aa33f
nemsissshah <nemsissshahbazian@gmail.com>
2022-12-19 01:34:50 -0800

    updated

--- config/config.php
+++ config/config.php
@@ -1,14 +1,14 @@
 <?php
 
-// define('DB_HOST', '303.itpwebdev.com');
-// define('DB_USER', 'nemsisss_db_user');
-// define('DB_PASS', 'uscitp2022');
-// define('DB_NAME', 'nemsisss_wishlists_db');
+define('DB_HOST', '303.itpwebdev.com');
+define('DB_USER', 'nemsisss_db_user');
+define('DB_PASS', 'uscitp2022');
+define('DB_NAME', 'nemsisss_wishlists_db');
 
-define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
-define('DB_USER', 'b17db2560e6dcf');
-define('DB_PASS', 'e6f7801e');
-define('DB_NAME', 'heroku_d4deb4b5f73d582');
+// define('DB_HOST', 'us-cdbr-east-06.cleardb.net');
+// define('DB_USER', 'b17db2560e6dcf');
+// define('DB_PASS', 'e6f7801e');
+// define('DB_NAME', 'heroku_d4deb4b5f73d582');
 session_start();
 
 ?>
